0cc738c253c211e7e4fd1517bc9ada1296ec5cf5
moemmia <moises.muniz.mangas@gmail.com>
2018-06-13 23:47:05 +0200

    :b:ress to start

---
+++ TestRoom/Assets/scripts/PressStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressStart : MonoBehaviour {
+
+    public GameObject canvasQuest;
+
+    // Use this for initialization
+    public void Click () {
+        canvasQuest.SetActive(true);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<ControladorDePreguntas>().enabled=true;
+       this.transform.parent.gameObject.SetActive(false);
+	}
+}
