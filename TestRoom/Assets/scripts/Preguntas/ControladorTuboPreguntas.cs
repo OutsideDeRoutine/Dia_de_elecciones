ae41600ed26323f86a210b31f22ae706afd7d907
moemmia <moises.muniz.mangas@gmail.com>
2018-06-16 00:36:34 +0200

    pantalla v02

--- TestRoom/Assets/scripts/Preguntas/ControladorTuboPreguntas.cs
+++ TestRoom/Assets/scripts/Preguntas/ControladorTuboPreguntas.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 
 public class ControladorTuboPreguntas : MonoBehaviour {
 
+
     IList<String> respuestas;
+    public Vector3 MoveToOnPaper;
+    public Canvas paper;
 	
     public void colocarFinalEnTubo(IList<String> respuesta)
     {
@@ -16,10 +19,31 @@ public class ControladorTuboPreguntas : MonoBehaviour {
     internal void mandarMensajeFinalJuego()
     {
         GameObject.FindGameObjectWithTag("Paper").GetComponent<Animator>().SetBool("open",true);
+        StartCoroutine(MoveCameraAndActiveScreen(1.5f));
     }
 
     public void finalizadaLecturaDeUnFinal()
     {
         //GetComponent<ControladorDePreguntas>().finalizadaLecturaDeUnFinal();
+    }
+
+    IEnumerator MoveCameraAndActiveScreen(float TimeBefore)
+    {
+        float time = Time.time;
+        yield return new WaitForSeconds(TimeBefore);
+        Camera.main.GetComponent<MoveCamera>().isScreen = true;
+        while (Vector3.Distance( this.transform.position, MoveToOnPaper)>0.1f)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, MoveToOnPaper, 0.1f);
+            Camera.main.transform.LookAt(paper.transform);
+            yield return new WaitForFixedUpdate();
+        }
+        float currentTime = Time.time;
+        Color bcolor = Camera.main.backgroundColor;
+        while (Time.time - currentTime < 0.5f)
+        {
+            paper.GetComponent<CanvasGroup>().alpha = Mathf.Abs(((Time.time - currentTime) / 0.5f));
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
---
+++ TestRoom/Assets/scripts/startSampleScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startSampleScene : MonoBehaviour {
+
+    void Start()
+    {
+        StartCoroutine(FadeIn(1.5f));
+        if (!Lenguaje.instance)
+        {
+            this.gameObject.AddComponent<Lenguaje>();
+            Lenguaje.instance.textLenguaje = "EN";
+        }
+    }
+    IEnumerator FadeIn(float time)
+    {
+        float currentTime = Time.time;
+        this.GetComponent<Canvas>().GetComponent<CanvasGroup>().alpha = 1;
+        while (Time.time - currentTime < time)
+        {
+            this.GetComponent<Canvas>().GetComponent<CanvasGroup>().alpha = 1- Mathf.Abs(((Time.time - currentTime) / time));
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
