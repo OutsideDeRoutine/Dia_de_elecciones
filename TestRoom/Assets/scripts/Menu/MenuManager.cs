391778711dc5b59b3f76bb2341f97af087938661
moemmia <moises.muniz.mangas@gmail.com>
2018-06-15 11:57:25 +0200

    fade in/out

--- TestRoom/Assets/scripts/Menu/MenuManager.cs
+++ TestRoom/Assets/scripts/Menu/MenuManager.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
+    public Canvas canvas;
+    void Start()
+    {
+        StartCoroutine(FadeIn(1.5f));
+    }
+    IEnumerator FadeIn(float time)
+    {
+        float currentTime = Time.time;
+        canvas.GetComponent<Canvas>().GetComponent<CanvasGroup>().alpha = 0;
+        while (Time.time - currentTime < time)
+        {
+            canvas.GetComponent<Canvas>().GetComponent<CanvasGroup>().alpha = Mathf.Abs(((Time.time - currentTime) / time));
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator FadeOut(string level, float time, Color color)
+    {
+        float currentTime = Time.time;
+        Color bcolor = Camera.main.backgroundColor;
+        while (Time.time - currentTime < time)
+        {
+            canvas.GetComponent<Canvas>().GetComponent<CanvasGroup>().alpha = Mathf.Abs(((Time.time - currentTime) / time) - 1);
+            Camera.main.backgroundColor = Color.Lerp(bcolor, color, (Time.time - currentTime) / time);
+            yield return new WaitForEndOfFrame();
+        }
+        SceneManager.LoadScene(level);
+    }
+
 
     public void empezarJuegoESP()
     {
         Lenguaje.instance.textLenguaje = "ES";
+
         empezarJuego();
     }
 
@@ -19,6 +49,6 @@ public class MenuManager : MonoBehaviour {
 
     private void empezarJuego()
     {
-        SceneManager.LoadScene("SampleScene");
+        StartCoroutine(FadeOut("SampleScene", 1f, Color.black));
     }
 }
